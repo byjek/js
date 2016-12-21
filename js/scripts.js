4bc93dbda67fbe58822a92156cc992f7a0d51991
Maciek <maciej.byjos@gmail.com>
2016-12-21 09:53:12 +0100

    scripts.js modify

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@
 var name = prompt('Wpisz swoje imiê');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+var a = 3;
+var h = 4;
+var triangleArea;
+triangleArea = 'a'*'h'/2;
+console.log('Pole trójk¹ta o podstawie a: ' + a + ' i wysokoœci h: ' + h + ' wynosi: ' + triangleArea);
