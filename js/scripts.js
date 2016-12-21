852973dc9d615e2ba5326211ca597a9432383e91
Maciek <maciej.byjos@gmail.com>
2016-12-21 10:43:55 +0100

    WTF

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-// plik scripts.js
-
-var name = prompt('Wpisz swoje imiê');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
-
 var a = 3;
-var h = 4;
-var triangleArea;
-triangleArea = 'a'*'h'/2;
-console.log('Pole trójk¹ta o podstawie a: ' + a + ' i wysokoœci h: ' + h + ' wynosi: ' + triangleArea);
+var b = 4;
+var value = (a*a) + (2 * a * b) - (b*b);
+console.log('value');
+
+if ('value'>=0) {
+console.log(‘Wynik dodatni’)
+} 
+if ('value'<0) {
+console.log(‘Wynik ujemny’)
+else if ('value'=0) {
+console.log(‘Zero’)
+}
+// nie mam pojêcia co w³aœnie zrobi³em :D
