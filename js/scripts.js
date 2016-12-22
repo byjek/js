ee3e2e182191e94b2caa29f6dcaf8736070dd6cd
byjek <maciej.byjos@gmail.com>
2016-12-22 08:57:33 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 var a = 3;
 var b = 4;
-var value = (a*a) + (2 * a * b) - (b*b);
+var value = (a*a) + (2 *a*b) - (b*b);
 console.log('value');
 
-if ('value'>0) {
-console.log(‘Wynik dodatni’)
+if (value>0) {
+console.log('Wynik dodatni')
 } 
-if ('value'<0) {
-console.log(‘Wynik ujemny’)
+else if (value<0) {
+console.log('Wynik ujemny')
 }
-else if ('value'=0) {
-console.log(‘Zero’)
+else (value==0) {
+console.log('Zero')
 }
-// nie mam pojêcia co w³aœnie zrobi³em :D
+// ;)
