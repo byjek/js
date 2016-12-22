d8578f4d86976e051e3ea37856059159ddaaf04e
byjek <maciej.byjos@gmail.com>
2016-12-22 08:53:28 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // plik scripts.js
 
-var name = prompt('Wpisz swoje imiê');
+var name = prompt('Wpisz swoje imiÄ™');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
 
 var a = 3;
 var h = 4;
 var triangleArea;
-triangleArea = 'a'*'h'/2;
-console.log('Pole trójk¹ta o podstawie a: ' + a + ' i wysokoœci h: ' + h + ' wynosi: ' + triangleArea);
+triangleArea = a*h/2;
+console.log('Pole trÃ³jkÄ…ta o podstawie a: ' + a + ' i wysokoÅ›ci h: ' + h + ' wynosi: ' + triangleArea);
